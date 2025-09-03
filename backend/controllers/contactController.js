0b5d6feceda2e563a4ac6506151d14c87fa93049
iobuyumbi <iobuyumbi@gmail.com>
2025-09-03 15:44:20 +0300

    chore: Corrected link to projects page

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -110,7 +110,7 @@ function sanitizeInput(input) {
  * Send contact email via nodemailer
  */
 async function sendContactEmail(data) {
-  const transporter = nodemailer.createTransporter({
+  const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT || 587,
     secure: false,
